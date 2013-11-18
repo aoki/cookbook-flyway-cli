8d7d5cc42dc5c975f361eb46cc67923406766db8
Yoshiki Aoki <ringohub@gmail.com>
2013-11-18 23:09:16 +0900

    version とりあえず完成

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,17 +7,32 @@
 # All rights reserved - Do Not Redistribute
 #
 
+install_dir = "/opt/flyway"
+version = "2.2.1"
+
+if version < "2.2" then
+  Chef::Log::error "#{cookbook_name} is supported versions in more than 2.2."
+  exit 1
+end
+
+directory "#{install_dir}/versions" do
+  owner     "root"
+  group     "wheel"
+  action    :create
+  recursive true
+end
+
 execute "flyway-cli" do
   command <<-EOH
-     wget http://repo1.maven.org/maven2/com/googlecode/flyway/flyway-commandline/2.2.1/flyway-commandline-2.2.1.tar.gz -P /tmp
-     tar xzfC /tmp/flyway-commandline-2.2.1.tar.gz /opt
-     ln -s /opt/flyway-2.2.1 /opt/flyway
+     wget http://repo1.maven.org/maven2/com/googlecode/flyway/flyway-commandline/#{version}/flyway-commandline-#{version}.tar.gz -P /tmp
+     tar xzfC /tmp/flyway-commandline-#{version}.tar.gz #{install_dir}/versions
+     ln -nfs #{install_dir}/versions/flyway-#{version} #{install_dir}/flyway
   EOH
 end
 
 execute "mysql-connector" do
   command <<-EOH
      wget http://cdn.mysql.com/Downloads/Connector-J/mysql-connector-java-5.1.26.tar.gz -P /tmp
-     tar xzfC /tmp/mysql-connector-java-5.1.26.tar.gz /opt/flyway/jars
+     tar xzfC /tmp/mysql-connector-java-5.1.26.tar.gz /opt/flyway/flyway/jars
   EOH
 end
